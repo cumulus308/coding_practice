673464b1c085ac5dd49c83de1e7e26a99fdb6651
cumulus308 <rhrudals312@naver.com>
2024-06-27 23:25:35 +0900

    Update update.py

--- update.py
+++ update.py
@@ -1,9 +1,9 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python
 
 import os
 from urllib import parse
 
-HEADER="""# 
+HEADER = """# 
 # 백준 & 프로그래머스 문제 풀이 목록
 
 프로그래머스의 경우, 푼 문제 목록에 대한 마이그레이션이 필요합니다.
@@ -14,8 +14,8 @@ def main():
     content = ""
     content += HEADER
     
-    directories = [];
-    solveds = [];
+    directories = []
+    solveds = []
 
     for root, dirs, files in os.walk("."):
         dirs.sort()
@@ -51,9 +51,11 @@ def main():
                 content += "|{}|[링크]({})|\n".format(category, parse.quote(os.path.join(root, file)))
                 solveds.append(category)
                 print("category : " + category)
+                print("file : " + file)
 
     with open("README.md", "w") as fd:
         fd.write(content)
-        
+        print("README.md 업데이트 완료")
+
 if __name__ == "__main__":
     main()
