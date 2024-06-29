07b69a1c37b4f31acede1c0cee42a2e0c38bb65c
cumulus308 <rhrudals312@naver.com>
2024-06-29 16:35:35 +0900

    [level 0] Title: 합성수 찾기, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.py
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.py
@@ -1,12 +1,8 @@
 def solution(n):
     count1 = 0
-    for i in range(n+1):
-        count2 = 0
-        for j in range(1,i+1):
+    for i in range(4,n+1):
+        for j in range(2,int(i**0.5)+1):
             if i % j ==0:
-                count2 += 1
-        if count2 >=3:
-            count1 += 1
-    
-    
+                count1 += 1  
+                break
     return count1
