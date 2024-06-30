7a75babe582d23426bc10e539c39c615515e29ef
cumulus308 <rhrudals312@naver.com>
2024-06-30 17:10:57 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.py
+++ 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.py
@@ -1,4 +1,12 @@
-def solution(n):
-    a = [x for x in range(200) if x % 3 != 0 and '3' not in str(x)]
+# def solution(n):
+#     a = [x for x in range(200) if x % 3 != 0 and '3' not in str(x)]
 
-    return a[n-1]
+#     return a[n-1]
+
+def solution(n):
+    answer = 0
+    for _ in range(n):
+        answer += 1
+        while answer % 3 == 0 or '3' in str(answer):
+            answer += 1
+    return answer
