27a605216032e7b52b73b425def94c6da0a8e0e9
cumulus308 <rhrudals312@naver.com>
2024-07-02 18:42:08 +0900

    [level 0] Title: 가위 바위 보, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.py
@@ -0,0 +1,13 @@
+def solution(rsp):
+    a = ""
+    for i in rsp:
+        if i == "2":
+            a += "0"
+        elif i == "0":
+            a += "5"
+        elif i == "5":
+            a += "2"
+            
+
+    
+    return a
