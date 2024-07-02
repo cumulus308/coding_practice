172d62d7c49aea15162b56705ce5e064db1d2064
cumulus308 <rhrudals312@naver.com>
2024-07-02 21:16:15 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.py
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.py
@@ -1,2 +1,10 @@
 def solution(my_string):
     return sorted(int(x) for x in my_string if x.isdigit())
+
+
+# def solution(my_string):
+#     answer = []
+#     for i in my_string:
+#         if i.isdigit():
+#             answer.append(int(i))
+#     return sorted(answer)
