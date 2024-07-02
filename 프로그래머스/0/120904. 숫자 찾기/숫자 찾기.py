6595d09b9eff492a613a973b839c4657e263adec
cumulus308 <rhrudals312@naver.com>
2024-07-02 18:53:08 +0900

    [level 0] Title: 숫자 찾기, Time: 0.00 ms, Memory: 9.99 MB -BaekjoonHub

--- 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.py
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.py
@@ -1,11 +1,17 @@
 def solution(num, k):
-    a= 0
-    b= list(str(num))
-    for i in b:
-        a+=1
-        if str(k) not in b:
-            return -1
+    for i, n in enumerate(str(num)):
+        if str(k) == n:
+            return i + 1
+    return -1
+
+# def solution(num, k):
+#     a= 0
+#     b= list(str(num))
+#     for i in b:
+#         a+=1
+#         if str(k) not in b:
+#             return -1
         
-        elif str(k) == i:
-            return a
-            break
+#         elif str(k) == i:
+#             return a
+#             break
