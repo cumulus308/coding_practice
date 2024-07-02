e4b7854a033cddf3a0b36934009cab947674d8a4
cumulus308 <rhrudals312@naver.com>
2024-07-02 18:54:15 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.py
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.py
@@ -1,4 +1,11 @@
 def solution(my_string):
-    a = list(my_string.lower())
-    a.sort()
-    return "".join(a)
+    return "".join(sorted(list(my_string.lower())))
+
+
+
+
+
+# def solution(my_string):
+#     a = list(my_string.lower())
+#     a.sort()
+#     return "".join(a)
