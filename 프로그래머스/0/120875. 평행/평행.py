e0f5f5c2313ae829b68a0f6c89aa078e4fe0105c
cumulus308 <rhrudals312@naver.com>
2024-07-03 17:08:11 +0900

    [level 0] Title: 평행, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/0/120875. 평행/평행.py
+++ 프로그래머스/0/120875. 평행/평행.py
@@ -1,11 +1,25 @@
 def solution(dots):
     a,b,c,d = dots
-    if (b[1]-a[1])/(b[0]-a[0]) ==(d[1]-c[1])/(d[0]-c[0]):
-        return 1
-    elif (c[1]-a[1])/(c[0]-a[0]) ==(d[1]-b[1])/(d[0]-b[0]):
-        return 1
-    elif (d[1]-a[1])/(d[0]-a[0]) ==(c[1]-b[1])/(c[0]-b[0]):
-        return 1
-    else:
-        return 0
+    return1 =(b[1]-a[1])/(b[0]-a[0]) ==(d[1]-c[1])/(d[0]-c[0])
+    return2 =(c[1]-a[1])/(c[0]-a[0]) ==(d[1]-b[1])/(d[0]-b[0]) 
+    return3 =(d[1]-a[1])/(d[0]-a[0]) ==(c[1]-b[1])/(c[0]-b[0])
+    return 1 if return1 or return2 or return3 else 0 
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+#     if (b[1]-a[1])/(b[0]-a[0]) ==(d[1]-c[1])/(d[0]-c[0]):
+#         return 1
+#     elif (c[1]-a[1])/(c[0]-a[0]) ==(d[1]-b[1])/(d[0]-b[0]):
+#         return 1
+#     elif (d[1]-a[1])/(d[0]-a[0]) ==(c[1]-b[1])/(c[0]-b[0]):
+#         return 1
+#     else:
+#         return 0
     
