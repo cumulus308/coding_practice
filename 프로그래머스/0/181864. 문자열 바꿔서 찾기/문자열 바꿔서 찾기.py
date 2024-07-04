01442fcddfa2b63c1e2479d204ecff39995abc2b
cumulus308 <rhrudals312@naver.com>
2024-07-04 22:03:37 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.py
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.py
@@ -1,10 +1,5 @@
 def solution(myString, pat):
-    # myString의 "A"를 "B"로, "B"를 "A"로 바꾸는 함수
-    def swap_ab(s):
-        return ''.join('B' if c == 'A' else 'A' for c in s)
-    
-    # myString을 변환
-    swapped = swap_ab(myString)
-    
-    # 변환된 문자열에 pat이 포함되어 있는지 확인
-    return 1 if pat in swapped else 0
+    myString = myString.replace("A","C")
+    myString = myString.replace("B","A")
+    myString = myString.replace("C","B")
+    return 1 if pat in myString else 0
