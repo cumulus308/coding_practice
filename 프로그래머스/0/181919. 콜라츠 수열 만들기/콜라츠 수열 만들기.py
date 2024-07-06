e0557c36d71d07f1744c6b26c4ff65b0b34438e1
cumulus308 <rhrudals312@naver.com>
2024-07-06 21:37:46 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.py
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.py
@@ -1,11 +1,9 @@
 def solution(n):
-    answer = []
+    answer = [n]
     while n > 1:
         if n % 2 == 0:
-            answer.append(n)
             n = n //2
         elif n % 2 == 1:
-            answer.append(n)
             n = 3*n+1
-    answer.append(n)
+        answer.append(n)
     return answer
