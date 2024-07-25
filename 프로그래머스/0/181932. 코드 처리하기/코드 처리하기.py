81514cb45725ad486778998765920402a27f1269
KyungMin Ko <rhrudals312@naver.com>
2024-07-25 17:58:50 +0900

    [level 0] Title: 코드 처리하기, Time: 15.04 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.py
@@ -0,0 +1,17 @@
+def solution(code):
+    mode = 0
+    ret = ""
+    for i in range(len(code)):
+        if mode == 0:
+            if code[i] == '1':
+                mode = 1
+            if i % 2 == 0 and code[i] != '1':
+                ret += code[i]
+        elif mode == 1:
+            if code[i] == '1':
+                mode = 0
+            if i % 2 == 1 and code[i] != '1':
+                ret += code[i]
+        
+                
+    return ret if ret else "EMPTY"
