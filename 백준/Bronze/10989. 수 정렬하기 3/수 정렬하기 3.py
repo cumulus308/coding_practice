7d9d19ba74e87432351e7471401896c39d0629b8
Kyungmin Ko <rhrudals312@naver.com>
2024-09-07 22:05:54 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 7924 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -0,0 +1,14 @@
+# 수 정렬하기 10989번
+import sys
+N = int(sys.stdin.readline())
+
+count = [0] * 10001
+
+for _ in range(N):
+    num = int(sys.stdin.readline())
+    count[num] += 1
+    
+for i in range(10001):
+    if count[i] > 0:
+        for _ in range(count[i]):
+            print(i)
