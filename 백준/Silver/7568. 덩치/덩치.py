f6e89901e00bf6281516dd702e115ce9f666a143
Kyungmin Ko <rhrudals312@naver.com>
2024-09-08 23:01:47 +0900

    [Silver V] Title: 덩치, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/7568. 덩치/덩치.py
@@ -0,0 +1,19 @@
+import sys
+
+N = int(input())
+people = []
+
+for _ in range(N):
+    weight, height = map(int, sys.stdin.readline().split())
+    people.append((weight, height))
+
+ranks = []
+for i in range(N):
+    rank = 1
+    for j in range(N):
+        if i != j:
+            if people[j][0] > people[i][0] and people[j][1] > people[i][1]:
+                rank += 1
+    ranks.append(rank)
+
+print(' '.join(map(str, ranks)))
