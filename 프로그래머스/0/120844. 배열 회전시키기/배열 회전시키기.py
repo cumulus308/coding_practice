4868b2f0878707dc52f48c1e95b0418b0c2cd865
Kyungmin Ko <rhrudals312@naver.com>
2024-09-13 14:29:52 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.py
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.py
@@ -1,3 +1,7 @@
 def solution(numbers, direction):
-    num = len(numbers)
-    return (numbers*2)[num-1:2*num-1] if direction == "right" else(numbers*2)[1:num+1]
+    if direction == "right":
+        answer = [numbers.pop()] + numbers
+    elif direction == "left":
+        value = numbers.pop(0)
+        answer = numbers +[value]
+    return answer
